47de3e267459030ff07165ae93ebb0c1a4dbf8e8
Matt Phillips <matt@mattphillips.io>
2022-09-26 10:32:17 +0100

    Upgrade example to latest version

--- examples/typescript/test/sum.test.ts
+++ examples/typescript/test/sum.test.ts
@@ -2,6 +2,7 @@ import sum from '../src/sum';
 
 describe('Test math', () => {
   it('should be equal 3', () => {
+    expect(1, 'one').not.toBe(2)
     expect(sum(1, 4), "it's not three").toBe(3);
   });
 
